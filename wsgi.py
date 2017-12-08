d7f00c3412f8adb6d0a93f58266b0f14fdc31751
Marvin Sinister <marvin-sinister@users.noreply.github.com>
2017-12-08 11:28:09 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,11 @@
+from random import randint
 from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    x = [randint(0,9) for p in range(0,987239478234879)]
+    return x
 
 if __name__ == "__main__":
     application.run()
